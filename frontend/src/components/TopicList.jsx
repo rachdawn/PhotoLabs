c5c1841c521571068c815989c75619b30829499c
Rachel Little <racheldawnlittle@gmail.com>
2023-12-06 18:15:39 -0700

    update to topics map and fixed unique id warning

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,13 +3,11 @@ import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
 const TopicList = (props) => {
-  const topicsArray = Object.keys(props).map((property) => {
-    return props[property];
-  });
+
   return (
     <span className="top-nav-bar__topic-list" >
-      {topicsArray.map((topic) => (
-        <TopicListItem key={topic.id} slug={topic.slug} title={topic.title} />
+      {props.topics.map((topic) => (
+        <TopicListItem key={topic.id} id={topic.id} slug={topic.slug} title={topic.title} getPhotoByTopic={props.getPhotoByTopic} />
       ))}
     </span>
   );
